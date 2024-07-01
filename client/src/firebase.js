6bcf53bde8a4113c7ff8a1c705d5ec9da3658c3f
Frank <franktaronna2002@gmail.com>
2024-07-01 17:29:15 +0200

    first commit

---
+++ client/src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "bytebeat-new-ddc53.firebaseapp.com",
+  projectId: "bytebeat-new-ddc53",
+  storageBucket: "bytebeat-new-ddc53.appspot.com",
+  messagingSenderId: "522552274084",
+  appId: "1:522552274084:web:da36019a637d302a194a31"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
+//import.meta.env.VITE_FIREBASE_API_KEY
